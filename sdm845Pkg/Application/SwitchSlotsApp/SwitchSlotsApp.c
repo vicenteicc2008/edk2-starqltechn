f87c9702cd0d04432a73bbd7d5a62fa6f5edcacb
Daniel6745 <90069111+Daniel224455@users.noreply.github.com>
2024-06-23 08:53:27 +0200

    Update SwitchSlotsApp.c

--- sdm845Pkg/Application/SwitchSlotsApp/SwitchSlotsApp.c
+++ sdm845Pkg/Application/SwitchSlotsApp/SwitchSlotsApp.c
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/
  */
 
-#include <Library/BootSlotLib.h>
+#include <Drivers/Op6tSlotDxe/Op6tSlotDxe.inf>
 #include <Library/UefiApplicationEntryPoint.h>
 
 EFI_STATUS
